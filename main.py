b956c1af34d7af81eaae0ba013f15acc7774aeec
vp79247 <70004405+vp79247@users.noreply.github.com>
2021-03-17 20:30:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 
 from __future__ import print_function
+import pandas as pd
 import os
 import gc
 import argparse
@@ -401,6 +402,7 @@ def train(args, net, train_loader, test_loader, boardio, textio):
 
         textio.cprint('==TRAIN==')
         textio.cprint('A--------->B')
+        print(EPOCH+'Train')
         textio.cprint('EPOCH:: %d, Loss: %f, Cycle Loss:, %f, MSE: %f, RMSE: %f, MAE: %f, rot_MSE: %f, rot_RMSE: %f, '
                       'rot_MAE: %f, trans_MSE: %f, trans_RMSE: %f, trans_MAE: %f'
                       % (epoch, train_loss, train_cycle_loss, train_mse_ab, train_rmse_ab, train_mae_ab, train_r_mse_ab,
@@ -412,6 +414,7 @@ def train(args, net, train_loader, test_loader, boardio, textio):
                          train_r_mae_ba, train_t_mse_ba, train_t_rmse_ba, train_t_mae_ba))
 
         textio.cprint('==TEST==')
+        print(EPOCH+'Test')
         textio.cprint('A--------->B')
         textio.cprint('EPOCH:: %d, Loss: %f, Cycle Loss: %f, MSE: %f, RMSE: %f, MAE: %f, rot_MSE: %f, rot_RMSE: %f, '
                       'rot_MAE: %f, trans_MSE: %f, trans_RMSE: %f, trans_MAE: %f'
@@ -424,6 +427,7 @@ def train(args, net, train_loader, test_loader, boardio, textio):
                          test_r_mae_ba, test_t_mse_ba, test_t_rmse_ba, test_t_mae_ba))
 
         textio.cprint('==BEST TEST==')
+        print(EPOCH+'best test')
         textio.cprint('A--------->B')
         textio.cprint('EPOCH:: %d, Loss: %f, Cycle Loss: %f, MSE: %f, RMSE: %f, MAE: %f, rot_MSE: %f, rot_RMSE: %f, '
                       'rot_MAE: %f, trans_MSE: %f, trans_RMSE: %f, trans_MAE: %f'
