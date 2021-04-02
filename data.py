03f8d6ec2dfdebb10a34faf0b50bec2cdf297695
vp79247 <70004405+vp79247@users.noreply.github.com>
2021-04-03 03:12:59 +0530

    Update data.py

--- data.py
+++ data.py
@@ -136,11 +136,11 @@ class ModelNet40(Dataset):
 
 if __name__ == '__main__':
     train = ModelNet40(1024)
-    train
+    print(train)
     #train1=pd.DataFrame(np.array(train))
     #train1.to_csv('train_modelNet40.csv')
     test = ModelNet40(1024, 'test')
-    test
+    print(test)
     #test1=pd.DataFrame(np.array(test))
     #test1.to_csv('test_modelNet40.csv')
     for data in train:
