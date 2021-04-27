fd10c2162493f860b5174a55d73d56698a70d8d9
vp79247 <70004405+vp79247@users.noreply.github.com>
2021-04-27 18:14:48 +0530

    Update data.py

--- data.py
+++ data.py
@@ -74,7 +74,7 @@ def jitter_pointcloud(pointcloud, sigma=0.01, clip=0.05):
     return pointcloud
 
 
-class ShapeNet(Dataset):
+class ModelNet40(Dataset):
     def __init__(self, num_points, partition='train', gaussian_noise=False, unseen=False, factor=4):
         self.train_data, self.test_data, self.train_label, self.test_label = load_data(partition)
         self.num_points = num_points
@@ -144,8 +144,8 @@ class ShapeNet(Dataset):
 
 
 if __name__ == '__main__':
-    train = ShapeNet(1024)
-    test = ShapeNet(1024, 'test')
+    train = ModelNet40(1024)
+    test = ModelNet40(1024, 'test')
     for data in train:
         print(len(data))
         break
