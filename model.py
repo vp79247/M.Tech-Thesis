8938e46e02104b02bd19324d6bf5648e30d88e43
vp79247 <70004405+vp79247@users.noreply.github.com>
2021-05-04 13:30:32 +0530

    Update model.py

--- model.py
+++ model.py
@@ -471,7 +471,7 @@ class DCP(nn.Module):
         else:
             rotation_ba = rotation_ab.transpose(2, 1).contiguous()
             translation_ba = -torch.matmul(rotation_ba, translation_ab.unsqueeze(2)).squeeze(2)
-        savetxt('rotation_ab.txt',rotation_ab,delimiter =', ')
-        savetxt('translation_ab.txt',rotation_ab,delimiter =', ')
+        savetxt('rotation_ab.txt',rotation_ab.cpu(),delimiter =', ')
+        savetxt('translation_ab.txt',rotation_ab.cpu(),delimiter =', ')
             
         return rotation_ab, translation_ab, rotation_ba, translation_ba
