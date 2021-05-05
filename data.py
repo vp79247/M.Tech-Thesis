5a3b0e9f37a0ee6bdcd9339ffff762539f0c0fe0
vp79247 <70004405+vp79247@users.noreply.github.com>
2021-05-05 17:25:33 +0530

    Update data.py

--- data.py
+++ data.py
@@ -113,7 +113,7 @@ class ShapeNet(Dataset):
     def __getitem__(self, item):
         
         pointcloud = self.data[item][:self.num_points]
-        p
+        
         if self.gaussian_noise:
             pointcloud = jitter_pointcloud(pointcloud)
         if self.partition != 'train':
