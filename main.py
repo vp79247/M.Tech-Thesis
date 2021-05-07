723e8edfddefef772d43fbae7ce8527ad71192af
vp79247 <70004405+vp79247@users.noreply.github.com>
2021-05-07 09:44:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -579,7 +579,7 @@ def main():
                         help='Size of batch)')
     parser.add_argument('--test_batch_size', type=int, default=10, metavar='batch_size',
                         help='Size of batch)')
-    parser.add_argument('--epochs', type=int, default=1, metavar='N',
+    parser.add_argument('--epochs', type=int, default=3, metavar='N',
                         help='number of episode to train ')
     parser.add_argument('--use_sgd', action='store_true', default=False,
                         help='Use SGD')
