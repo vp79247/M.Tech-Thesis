514675cf56821c34017efca73cc54b796b064dd4
vp79247 <70004405+vp79247@users.noreply.github.com>
2021-05-07 09:19:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -377,6 +377,24 @@ def train(args, net, train_loader, test_loader, boardio, textio):
         test_t_rmse_ba = np.sqrt(test_t_mse_ba)
         test_t_mae_ba = np.mean(np.abs(test_translations_ba - test_translations_ba_pred))
         
+        train_rmse_out=train_rmse_out.append(train_rmse_ab)
+        test_rmse_out=test_rmse_out.append(test_rmse_ab)
+        train_rot_rmse_out=train_rot_rmse_out.append(train_r_rmse_ab)
+        test_rot_rmse_out=test_rot_rmse_out.append(test_r_rmse_ab)
+        train_trans_rmse_out=train_trans_rmse_out.append(test_t_rmse_ab)
+        test_trans_rmse_out=test_trans_rmse_out.append(test_t_rmse_ba)
+        #epoch_out=epoch_out.append(epoch %d)
+        
+        df=pd.DataFrame(columns=['train_rmse','test_rmse','train_rot_rmse','test_rot_rmse','train_trans_rmse','test_trans_rmse'])
+        #df['epochs']=np.array(epoch_out)
+        df['train_rmse']=np.array(train_rmse_out)
+        df['test_rmse']=np.array(test_rmse_out)
+        df['train_rot_rmse']=np.array(train_rot_rmse_out)
+        df['test_rot_rmse']=np.array(test_rot_rmse_out)
+        df['train_trans_rmse']=np.array(train_trans_rmse_out)
+        df['test_trans_rmse']=np.array(test_trans_rmse_out)
+        
+        
         
         if best_test_loss >= test_loss:
             best_test_loss = test_loss
@@ -411,23 +429,7 @@ def train(args, net, train_loader, test_loader, boardio, textio):
             else:
                 torch.save(net.state_dict(), 'checkpoints/%s/models/model.best.t7' % args.exp_name)
                 
-        train_rmse_out=train_rmse_out.append(train_rmse_ab)
-        test_rmse_out=test_rmse_out.append(test_rmse_ab)
-        train_rot_rmse_out=train_rot_rmse_out.append(train_r_rmse_ab)
-        test_rot_rmse_out=test_rot_rmse_out.append(test_r_rmse_ab)
-        train_trans_rmse_out=train_trans_rmse_out.append(test_t_rmse_ab)
-        test_trans_rmse_out=test_trans_rmse_out.append(test_t_rmse_ba)
-        #epoch_out=epoch_out.append(epoch %d)
         
-        df=pd.DataFrame(columns=['train_rmse','test_rmse','train_rot_rmse','test_rot_rmse','train_trans_rmse','test_trans_rmse'])
-        #df['epochs']=np.array(epoch_out)
-        df['train_rmse']=np.array(train_rmse_out)
-        df['test_rmse']=np.array(test_rmse_out)
-        df['train_rot_rmse']=np.array(train_rot_rmse_out)
-        df['test_rot_rmse']=np.array(test_rot_rmse_out)
-        df['train_trans_rmse']=np.array(train_trans_rmse_out)
-        df['test_trans_rmse']=np.array(test_trans_rmse_out)
-        df.to_csv('df.csv')
 
 
         textio.cprint('==TRAIN==')
@@ -545,7 +547,7 @@ def train(args, net, train_loader, test_loader, boardio, textio):
         else:
             torch.save(net.state_dict(), 'checkpoints/%s/models/model.%d.t7' % (args.exp_name, epoch))
         gc.collect()
-    
+    df.to_csv('df.csv')
 
 def main():
     parser = argparse.ArgumentParser(description='Point Cloud Registration')
