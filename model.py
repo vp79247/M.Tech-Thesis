30719b39dc2d5f192c7e6d0eaf6a0826382cbd42
vp79247 <70004405+vp79247@users.noreply.github.com>
2021-07-03 23:00:28 +0530

    Update model.py

--- model.py
+++ model.py
@@ -445,7 +445,7 @@ class SVDHead(nn.Module):
         error=0
         for i in range(100):
             src_corr1=torch.matmul(R,src)+t
-            error=torch.mode(src_corr1-src_corr)
+            error=torch.sum(torch.sum(torch.mode(src_corr1-src_corr),1))
             if error>0:
                 src=src_corr1
                 src_centered = src - src.mean(dim=2, keepdim=True)
