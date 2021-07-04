4b601051810562cf55a32ecf87dc22509773e616
vp79247 <70004405+vp79247@users.noreply.github.com>
2021-07-04 10:57:31 +0530

    Update model.py

--- model.py
+++ model.py
@@ -501,7 +501,27 @@ class DCP(nn.Module):
         ##ICP##
         
         
+           #iter1 
+        src_transformed=transform_point_cloud(src, rotation_ab, translation_ab)
+            #src_embedding_trans=transform_point_cloud(src_embedding, rotation_ab, translation_ab)
+        e=torch.abs(src_transformed-tgt)
+           
+        error=torch.sum(torch.sum(e,1))
+        if error>0:
+            src=src_transformed
             
+        rotation_ab, translation_ab = self.head(src_embedding, tgt_embedding, src, tgt)
+        #iter 2
+        src_transformed=transform_point_cloud(src, rotation_ab, translation_ab)
+            #src_embedding_trans=transform_point_cloud(src_embedding, rotation_ab, translation_ab)
+        e=torch.abs(src_transformed-tgt)
+           
+        error=torch.sum(torch.sum(e,1))
+        if error>0:
+            src=src_transformed
+            
+        rotation_ab, translation_ab = self.head(src_embedding, tgt_embedding, src, tgt)
+        #iter 3
         src_transformed=transform_point_cloud(src, rotation_ab, translation_ab)
             #src_embedding_trans=transform_point_cloud(src_embedding, rotation_ab, translation_ab)
         e=torch.abs(src_transformed-tgt)
@@ -512,6 +532,18 @@ class DCP(nn.Module):
             
         rotation_ab, translation_ab = self.head(src_embedding, tgt_embedding, src, tgt)
             
+            
+            #iter 4
+            
+        src_transformed=transform_point_cloud(src, rotation_ab, translation_ab)
+            #src_embedding_trans=transform_point_cloud(src_embedding, rotation_ab, translation_ab)
+        e=torch.abs(src_transformed-tgt)
+           
+        error=torch.sum(torch.sum(e,1))
+        if error>0:
+            src=src_transformed
+            
+        rotation_ab, translation_ab = self.head(src_embedding, tgt_embedding, src, tgt)
 
         
         if self.cycle:
