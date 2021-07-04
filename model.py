32724a9ec9a7ed908940834d3063b5e6d96590c6
vp79247 <70004405+vp79247@users.noreply.github.com>
2021-07-04 10:46:11 +0530

    Update model.py

--- model.py
+++ model.py
@@ -499,18 +499,18 @@ class DCP(nn.Module):
         #print(tgt.shape)
         
         ##ICP##
-        #error=0
-        #for i in range(5):
+        
+        
             
-            #src_transformed=transform_point_cloud(src, rotation_ab, translation_ab)
+        src_transformed=transform_point_cloud(src, rotation_ab, translation_ab)
             #src_embedding_trans=transform_point_cloud(src_embedding, rotation_ab, translation_ab)
-            #e=torch.abs(src_transformed-tgt)
+        e=torch.abs(src_transformed-tgt)
            
-            #error=torch.sum(torch.sum(e,1))
-            #if error>0:
-                #src=src_transformed
+        error=torch.sum(torch.sum(e,1))
+        if error>0:
+            src=src_transformed
             
-            #rotation_ab, translation_ab = self.head(src_embedding, tgt_embedding, src, tgt)
+        rotation_ab, translation_ab = self.head(src_embedding, tgt_embedding, src, tgt)
             
 
         
